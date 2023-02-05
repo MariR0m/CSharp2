3f6dfb3cd029f3eca59beacc4c754c60103c43c4
MariR0m <122567747+MariR0m@users.noreply.github.com>
2023-02-06 01:52:13 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-System.Console.Write("Введите трехзначное число: ");
+
+
+// if (number < 100)
+//  {
+//     Console.WriteLine("Третьей цифры нет.");
+//  }
+
+// while (number > 999)
+// {
+//     number = number / 10;
+// }
+// number = number % 10;
+
+// System.Console.Write("Третья цифра введенного числа: " + number);
+
+System.Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
- if (number < 100 || number > 999)
+while (number > 999)
+{
+    number = number / 10;
+}
+
+if (number < 100)
  {
-    Console.WriteLine("Ошибка. Введено не трехзначное число.");
+    Console.WriteLine("Третьей цифры нет.");
  }
 
-int a = (number / 10) % 10;
- 
-System.Console.Write("Вторая цифра введенного числа: " + a);
+ else if (number > 100)
+ {
+    number = number % 10;
+    System.Console.Write("Третья цифра введенного числа: " + number);
+ }
